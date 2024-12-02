7a36a7f4ae567fa96ffb437d66825e6c2445845f
Amamiyakun02 <amamiyakun02@gmail.com>
2024-12-02 15:57:15 +0800

    add main program

--- App/main.py
+++ App/main.py
@@ -1,3 +1,30 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, Response
+from uvicorn import Server
+
+app: FastAPI = FastAPI(
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+    openapi_url="/api/openapi.json",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+@app.get('')
+async def index():
+    data = {
+        'message' : 'Hello World!',
+        'success': True,
+    }
+    return Response(content=data, status_code=200)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
